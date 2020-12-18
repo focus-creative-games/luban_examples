4280ae8233f832849ede685b138681cf71cf1059
walon <taojingjian@gmail.com>
2020-12-18 19:44:37 +0800

    【新增】新增 Lua_Unity_tolua_lua 项目

---
+++ Projects/Lua_Unity_tolua_lua/Assets/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LuaInterface;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaState lua = new LuaState();
+        lua.Start();
+        LuaBinder.Bind(lua);
+        string main = @"require 'Main'; Main()";
+
+        lua.DoString(main, "Main.cs");
+        lua.CheckTop();
+        lua.Dispose();
+        lua = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
