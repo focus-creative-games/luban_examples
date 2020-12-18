29b96756ca8a0f575356b06662b74a0600c85d91
walon <taojingjian@gmail.com>
2020-12-18 17:01:32 +0800

    【新增】添加 Csharp_Unity_ILRuntime_bin 项目

---
+++ Projects/Csharp_Unity_ILRuntime_bin/HotFix_Project/InstanceClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Bright.Serialization;
+using UnityEngine;
+
+namespace HotFix_Project
+{
+    public class InstanceClass
+    {
+        private int id;
+
+        public InstanceClass()
+        {
+            UnityEngine.Debug.Log("!!! InstanceClass::InstanceClass()");
+            this.id = 0;
+        }
+
+        public InstanceClass(int id)
+        {
+            UnityEngine.Debug.Log("!!! InstanceClass::InstanceClass() id = " + id);
+            this.id = id;
+        }
+
+        public int ID
+        {
+            get { return id; }
+        }
+
+        // static method
+        public static void StaticFunTest()
+        {
+            UnityEngine.Debug.Log("!!! InstanceClass.StaticFunTest()");
+
+            var tables = new cfg.Tables(file => new ByteBuf(File.ReadAllBytes(Application.dataPath + "/../config_data/" + file)));
+            UnityEngine.Debug.Log("== load succ ==");
+        }
+
+        public static void StaticFunTest2(int a)
+        {
+            UnityEngine.Debug.Log("!!! InstanceClass.StaticFunTest2(), a=" + a);
+        }
+
+        public static void GenericMethod<T>(T a)
+        {
+            UnityEngine.Debug.Log("!!! InstanceClass.GenericMethod(), a=" + a);
+        }
+
+        public void RefOutMethod(int addition, out List<int> lst, ref int val)
+        {
+            val = val + addition + id;
+            lst = new List<int>();
+            lst.Add(id);
+        }
+    }
+
+
+}
