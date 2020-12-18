9ab5501eb2832c0e49d72727f4cbbbc64aa92992
walon <taojingjian@gmail.com>
2020-12-18 15:59:46 +0800

    【新增】 新增 Csharp_Unity_bin 项目

---
+++ Projects/Csharp_Unity_bin/Assets/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Bright.Serialization;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        new cfg.Tables(LoadByteBuf);
+        UnityEngine.Debug.Log("== load succ==");
+    }
+
+    private static ByteBuf LoadByteBuf(string file)
+    {
+        return new ByteBuf(File.ReadAllBytes(Application.dataPath + "/../config_data/" + file));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
