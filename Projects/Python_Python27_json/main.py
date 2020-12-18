4296949959770ef6c0df643006dd0282bedd3c36
walon <taojingjian@gmail.com>
2020-12-18 19:42:33 +0800

    【新增】新增 Python_Python27_json 项目

---
+++ Projects/Python_Python27_json/main.py
@@ -0,0 +1,11 @@
+import json 
+import gen.Types
+def loader(f):
+    return json.load(open('config_data/' + f, 'r'))
+
+tables = gen.Types.Tables(loader)
+
+print(tables)
+
+r = tables.TbItem.getDataList()[0]
+print(dir(r))
