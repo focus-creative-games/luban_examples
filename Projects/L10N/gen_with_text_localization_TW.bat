095f1f7233f11aa06ecfea25b0644a6f04994ee2
walon <taojingjian@gmail.com>
2021-06-18 17:14:59 +0800

    添加本地化相关示例项目

---
+++ Projects/L10N/gen_with_text_localization_TW.bat
@@ -0,0 +1,18 @@
+set WORKSPACE=..\..
+set GEN_CLIENT=%WORKSPACE%\Tools\Luban.Client\Luban.Client.exe
+set CONF_ROOT=%WORKSPACE%\DesignerConfigs
+set DEFINE_FILE=%CONF_ROOT%\Defines\__root__.xml
+
+%GEN_CLIENT% -h %LUBAN_SERVER_IP% -j cfg --^
+ -d %DEFINE_FILE%^
+ --input_data_dir %CONF_ROOT%\Datas ^
+ --output_code_dir Gen ^
+ --output_data_dir config_data ^
+ --gen_types data_lua ^
+ -s all ^
+--export_test_data ^
+--input_l10n_text_files l10n/TextTable_CN.xlsx ^
+--l10n_text_field_name text_tw ^
+--output_l10n_not_converted_text_file NotLocalized_CN.txt
+
+pause
