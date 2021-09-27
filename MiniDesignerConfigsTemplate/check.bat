3250f00ced89ff1660854225384da33d1d938dee
walon <taojingjian@gmail.com>
2021-09-27 13:54:39 +0800

    新增 rust json支持

--- MiniDesignerConfigsTemplate/check.bat
+++ MiniDesignerConfigsTemplate/check.bat
@@ -3,8 +3,9 @@ set GEN_CLIENT=dotnet ..\Tools\Luban.ClientServer\Luban.ClientServer.dll
 %GEN_CLIENT% -j cfg --^
  -d Defines\__root__.xml ^
  --input_data_dir Datas ^
- --output_data_dir ./output_json ^
- --gen_types data_json ^
+ --output_data_dir output_json ^
+ --output_code_dir gen ^
+ --gen_types code_cs_unity_json,data_json ^
  -s all ^
 --export_test_data
 pause
---
+++ Projects/Rust_Json/src/main.rs
@@ -0,0 +1,18 @@
+
+mod cfg;
+
+fn main() {    
+
+    let tables = match cfg::Tables::new(|x:&str| {
+        let full_path = "../GenerateDatas/json/".to_string() + x + ".json";
+        println!("{}", full_path);
+        json::parse(&match std::fs::read_to_string(&full_path) {
+        Ok(content) => content,
+        Err(_) => return Err(cfg::LoadError{}),
+    }).or_else(|__e| {Err(cfg::LoadError{})})}) {
+        Ok(t) => t,
+        Err(_) => return
+    };
+    
+    println!("Hello, world!, {}", tables.tbglobalconfig.get_data().bag_capacity);
+}
