08e3f595d3550f95675fdc1a6ef30f92c6a1b82d
walon <taojingjian@gmail.com>
2021-11-05 11:32:55 +0800

    修复 convert_xlsx 在mac平台无法生成的问题

--- Tools/build_excel2textdiff.bat
+++ Tools/build_excel2textdiff.bat
@@ -1,5 +1,7 @@
-set BIN=%CD%/Excel2TextDiff
-cd /d D:\workspace\Excel2TextDiff
+set APP=Excel2TextDiff
+rd /s /q %APP%
+set BIN=%CD%/%APP%
+cd /d D:\workspace\RarelyUse\%APP%
 call dotnet build -c release -o %BIN%
 
 pause
--- Tools/build_luban_assistant.bat
+++ Tools/build_luban_assistant.bat
@@ -1,3 +1,4 @@
+rd /s /q LubanAssistant
 set BIN=%CD%/LubanAssistant
 cd /d D:\workspace\luban\src\LubanAssistant
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe" LubanAssistant.csproj /p:OutputPath=%BIN% /property:Configuration=Release
