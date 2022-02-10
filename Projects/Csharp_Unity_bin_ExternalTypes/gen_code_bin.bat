9ef38c3eda6cc0d66c9176c26b7f300ac55c0bc9
walon <taojingjian@gmail.com>
2022-02-10 14:01:34 +0800

    调整 externaltype设计

--- Projects/Csharp_Unity_bin_ExternalTypes/gen_code_bin.bat
+++ Projects/Csharp_Unity_bin_ExternalTypes/gen_code_bin.bat
@@ -8,7 +8,7 @@ set CONF_ROOT=%WORKSPACE%\DesignerConfigs
  -d %CONF_ROOT%\Defines\__root__.xml ^
  --input_data_dir %CONF_ROOT%\Datas ^
  --output_code_dir Assets/Gen ^
- --output_data_dir ..\GenerateDatas\bin ^
+ --output_data_dir ..\GenerateDatas\bytes ^
  --gen_types code_cs_bin,data_bin ^
  --external:selectors unity_cs ^
  -s all 
