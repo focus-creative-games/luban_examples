1d467cf28c02dc78bf49d50c63f9bba18039f631
walon <taojingjian@gmail.com>
2022-05-06 11:14:11 +0800

    新增 Csharp_Unity_bin_or_json 项目。演示如何自动根据生成的bin或json格式对应的代码，自适应加载相应配置。

---
+++ Projects/Csharp_Unity_bin_or_json/Assets/Main.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Bright.Serialization;
+using UnityEngine;
+using SimpleJSON;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tablesCtor = typeof(cfg.Tables).GetConstructors()[0];
+        var loaderReturnType = tablesCtor.GetParameters()[0].ParameterType.GetGenericArguments()[1];
+        // 根据cfg.Tables的构造函数的Loader的返回值类型决定使用json还是ByteBuf Loader
+        System.Delegate loader = loaderReturnType == typeof(ByteBuf) ?
+            new System.Func<string, ByteBuf>(LoadByteBuf)
+            : (System.Delegate)new System.Func<string, JSONNode>(LoadJson);
+        var tables = (cfg.Tables)tablesCtor.Invoke(new object[] {loader});
+
+        UnityEngine.Debug.LogFormat("item[1].name:{0}", tables.TbItem[1].Name);
+        Debug.LogFormat("bag init capacity:{0}", tables.TbGlobalConfig.BagInitCapacity);
+
+        //tables.TranslateText((key, text) => text + "# translate");
+
+        Dictionary<(int, int), string> m = new Dictionary<(int, int), string>();
+
+        m.Add((1, 2), "xxx");
+
+        UnityEngine.Debug.Log("== load succ==");
+    }
+
+    private static JSONNode LoadJson(string file)
+    {
+        return JSON.Parse(File.ReadAllText($"{Application.dataPath}/../../GenerateDatas/json/{file}.json", System.Text.Encoding.UTF8));
+    }
+
+    private static ByteBuf LoadByteBuf(string file)
+    {
+        return new ByteBuf(File.ReadAllBytes($"{Application.dataPath}/../../GenerateDatas/bytes/{file}.bytes"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Projects/Csharp_Unity_bin_or_json/gen_code_json.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+WORKSPACE=../..
+
+GEN_CLIENT=${WORKSPACE}/Tools/Luban.Client/Luban.Client.dll
+CONF_ROOT=${WORKSPACE}/DesignerConfigs
+
+
+dotnet ${GEN_CLIENT} -h ${LUBAN_SERVER_IP} -j cfg --\
+ -d ${CONF_ROOT}/Defines/__root__.xml \
+ --input_data_dir ${CONF_ROOT}/Datas \
+ --output_code_dir Assets/Gen \
+ --output_data_dir ../GenerateDatas/json \
+ --gen_types code_cs_unity_json,data_json \
+ -s all 
