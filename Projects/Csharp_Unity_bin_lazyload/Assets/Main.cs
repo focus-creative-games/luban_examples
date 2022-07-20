e2e4c9f88dfd73379cf7213a345cb5a89f4ebf07
Carson Lin <396098651@qq.com>
2022-07-20 10:48:40 +0800

    feat:添加二进制延迟加载示例
    Signed-off-by: Carson Lin <396098651@qq.com>

---
+++ Projects/Csharp_Unity_bin_lazyload/Assets/Main.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Bright.Serialization;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tables = new cfg.Tables(LoadIdxByteBuf, LoadDataByteBuf);
+
+
+        Debug.Log(tables.TbGlobalConfig.DamageParamD);
+        Debug.Log(tables.TbMultiUnionIndexList.Get(1, 1, "ab1").Desc);
+        Debug.Log(tables.TbNotIndexList.Get(2).X);
+        Debug.Log(tables.TbNotIndexList.DataList.Count);
+        foreach (var item in tables.TbNotIndexList.DataList)
+        {
+            Debug.Log($"======{item.X},{item.Y}======");
+        }
+    }
+
+    private static ByteBuf LoadIdxByteBuf(string file)
+    {
+        return new ByteBuf(File.ReadAllBytes($"{Application.dataPath}/../../GenerateDatas/bidx/{file}.bytes"));
+    }
+    private static ByteBuf LoadDataByteBuf(string file)
+    {
+        return new ByteBuf(File.ReadAllBytes($"{Application.dataPath}/../../GenerateDatas/bytes/{file}.bytes"));
+    }
+}
