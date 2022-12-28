fe99e03b544936e53aa86effe501e3bb96ae1821
Skyhand <2269721300@qq.com>
2022-12-28 10:50:02 +0800

    add template_search_path

--- Projects/Csharp_CustomTemplate_AsyncLoad/gen_code_json.bat
+++ Projects/Csharp_CustomTemplate_AsyncLoad/gen_code_json.bat
@@ -3,7 +3,7 @@ set WORKSPACE=..\..
 set GEN_CLIENT=%WORKSPACE%\Tools\Luban.ClientServer\Luban.ClientServer.exe
 set CONF_ROOT=%WORKSPACE%\DesignerConfigs
 
-%GEN_CLIENT% -j cfg --^
+%GEN_CLIENT% -t CustomTemplate -j cfg --^
  -d %CONF_ROOT%\Defines\__root__.xml ^
  --input_data_dir %CONF_ROOT%\Datas ^
  --output_code_dir Gen ^
--- Projects/Csharp_CustomTemplate_AsyncLoad/gen_code_json.sh
+++ Projects/Csharp_CustomTemplate_AsyncLoad/gen_code_json.sh
@@ -4,7 +4,7 @@ WORKSPACE=../..
 GEN_CLIENT=${WORKSPACE}/Tools/Luban.ClientServer/Luban.ClientServer.dll
 CONF_ROOT=${WORKSPACE}/DesignerConfigs
 
-dotnet ${GEN_CLIENT} -j cfg --\
+dotnet ${GEN_CLIENT} -t CustomTemplate -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_code_dir Gen \
