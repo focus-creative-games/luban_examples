ef32122954ca3dff411c490bf27129d002cd07e0
walon <walon@code-philosophy.com>
2023-08-20 12:16:35 +0800

    更新Luban，支持cpp-bin

---
+++ Projects/Cpp_bin/gen.bat
@@ -0,0 +1,14 @@
+set WORKSPACE=..\..
+
+set GEN_CLIENT=%WORKSPACE%\Tools\Luban.ClientServer\Luban.ClientServer.exe
+set CONF_ROOT=%WORKSPACE%\DesignerConfigs
+
+%GEN_CLIENT% -j cfg --^
+ -d %CONF_ROOT%\Defines\__root__.xml ^
+ --input_data_dir %CONF_ROOT%\Datas ^
+ --output_code_dir Gen ^
+ --output_data_dir ..\GenerateDatas\bytes ^
+ --gen_types code_cpp_bin,data_bin ^
+ -s all 
+
+pause
---
+++ Projects/Cpp_bin/luban/ByteBuf.cpp
@@ -0,0 +1,7 @@
+#include "ByteBuf.h"
+
+namespace luban
+{
+    const int ByteBuf::INIT_CAPACITY = 16;
+    const byte ByteBuf::EMPTY_BYTES[1] = { '\0' };
+}
