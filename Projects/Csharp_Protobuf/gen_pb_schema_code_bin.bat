f9850c7a05be1bd1ece4d2cc94fc7af956b023a9
absences <49706424+absences@users.noreply.github.com>
2024-07-03 21:02:08 +0800

    add cs-protobuf example (#32)

---
+++ Projects/Csharp_Protobuf/gen_pb_schema_code_bin.bat
@@ -0,0 +1,18 @@
+set WORKSPACE=..\..
+
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=%WORKSPACE%\DataTables
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -c cs-protobuf3 ^
+    -d protobuf-bin  ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=pb_schemas ^
+    -x outputDataDir=pb_datas ^
+    -x pathValidator.rootDir=%WORKSPACE%\Projects\Csharp_Unity_bin ^
+    -x l10n.provider=default ^
+    -x l10n.textFile.path=*@%WORKSPACE%\DataTables\Datas\l10n\texts.json ^
+    -x l10n.textFile.keyFieldName=key
+
+pause
