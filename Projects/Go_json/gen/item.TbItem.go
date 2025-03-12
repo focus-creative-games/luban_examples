9f44a1fcae1650e7d8cf487856595a1deb5ce763
loushizan <loushizan@dobest.com>
2025-03-12 14:47:17 +0800

    style(gen): 格式化 item.TbItem.go 文件
    - 使用了正确的缩进（4个空格）

--- Projects/Go_json/gen/item.TbItem.go
+++ Projects/Go_json/gen/item.TbItem.go
@@ -12,41 +12,41 @@ var ItemTbItemLoader func(*ItemItem)
 var ItemTbItemPostLoader func([]*ItemItem)
 
 type ItemTbItem struct {
-	_dataMap  map[int32]*ItemItem
-	_dataList []*ItemItem
+    _dataMap  map[int32]*ItemItem
+    _dataList []*ItemItem
 }
 
 func NewItemTbItem(_buf []map[string]interface{}) (*ItemTbItem, error) {
-	_dataList := make([]*ItemItem, 0, len(_buf))
-	dataMap := make(map[int32]*ItemItem)
+    _dataList := make([]*ItemItem, 0, len(_buf))
+    dataMap := make(map[int32]*ItemItem)
 
-	for _, _ele_ := range _buf {
-		if _v, err2 := NewItemItem(_ele_); err2 != nil {
-			return nil, err2
-		} else {
-			if ItemTbItemLoader != nil {
-				ItemTbItemLoader(_v)
-			}
+    for _, _ele_ := range _buf {
+        if _v, err2 := NewItemItem(_ele_); err2 != nil {
+            return nil, err2
+        } else {
+            if ItemTbItemLoader != nil {
+                ItemTbItemLoader(_v)
+            }
 
-			_dataList = append(_dataList, _v)
-			dataMap[_v.Id] = _v
-		}
-	}
+            _dataList = append(_dataList, _v)
+            dataMap[_v.Id] = _v
+        }
+    }
 
-	if ItemTbItemPostLoader != nil {
-		ItemTbItemPostLoader(_dataList)
-	}
-	return &ItemTbItem{_dataList: _dataList, _dataMap: dataMap}, nil
+    if ItemTbItemPostLoader != nil {
+        ItemTbItemPostLoader(_dataList)
+    }
+    return &ItemTbItem{_dataList: _dataList, _dataMap: dataMap}, nil
 }
 
 func (table *ItemTbItem) GetDataMap() map[int32]*ItemItem {
-	return table._dataMap
+    return table._dataMap
 }
 
 func (table *ItemTbItem) GetDataList() []*ItemItem {
-	return table._dataList
+    return table._dataList
 }
 
 func (table *ItemTbItem) Get(key int32) *ItemItem {
-	return table._dataMap[key]
+    return table._dataMap[key]
 }
